4e8b1abcae560dfb66e1da6d8701a5dd4d683143
James <guoanl@outlook.com>
2024-04-06 13:05:26 +0800

    Apply some effects to UTAS

--- src/component/About.js
+++ src/component/About.js
@@ -5,7 +5,7 @@ const About = ()=>{
         <>
         <div id="ABOUT" className={classNames("font-Inter font-medium dark:text-slate-400 mb-24 text-lg text-justify lg:p-4")}>
         <h1 className={classNames("font-Inter mb-3 lg:pt-4 py-2 lg:pb-4 pr-4 dark:text-white font-bold sticky lg:relative top-0 dark:bg-slate-900/[.05] backdrop-blur-md")}>ABOUT</h1>
-        <p className="mb-3 leading-8">Hey! My name is James, I was graduated from the University of Tasmania, majoring in Information Technology and Systems.</p>
+        <p className="mb-3 leading-8">Hey! My name is James, I was graduated from the <a href="http://www.utas.edu.au" className="ltrEffect">University of Tasmania</a>, majoring in <a href="https://www.utas.edu.au/courses/cse/courses/k7i-master-of-information-technology-and-systems?year=2023" className="ltrEffect">Information Technology and Systems.</a></p>
         <p className="mb-3 leading-8">I'm a software engineer specializing in building accessible digital experiences. Currently, I'm focused on building accessible, human-centered products at <a href="http://www.cqu.edu.cn" className="ltrEffect">Chongqing University.</a></p>
         <p className="mb-3 leading-8">When I'm not on the computer, I enjoy video games, travelling, and petting dogs.</p>
         </div>
